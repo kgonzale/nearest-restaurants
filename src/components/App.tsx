d87ee9496be40a9a68aefe74b151978972255dab
unknown <kgonzale@cbu.edu>
2019-04-20 21:14:11 -0500

    fixed index and app

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -4,10 +4,14 @@ import DefaultPage from './DefaultPage';
 import Results from './Results';
 
 const App = () => {
+
+    return (
     <Router>
         <Route exact path="/" component={DefaultPage}/>
         <Route exact path="/results" component={Results} />
     </Router>
+    );
+    
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Main from './components/Main';
+import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<Main />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
